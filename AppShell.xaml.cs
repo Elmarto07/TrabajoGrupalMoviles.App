32ea0aaeb57a1f45659ca99f4098bfaf568f8347
Elmarto07 <73851846+Elmarto07@users.noreply.github.com>
2023-06-08 21:50:36 +0200

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TrabajoGrupalMoviles;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace TrabajoGrupalMoviles;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Contador.Text = countButton.Value.ToString();
+
+    }
+
+	private void onResetCount(object sender, EventArgs e)
+	{
+        resetValue.Text = Contador.Text;
+    }
+
+	private void onClickCheckBox(object sender, EventArgs e)
+	{
+		if (changeNumberSimbol.IsChecked)
+		{
+            countButton.Minimum = -999;
+		}
+		else
+		{
+            countButton.Minimum = 0;
+        }
+	}
+}
+
